1c1201324a534565b5ffe675e3e0648ced7bd6e5
afreet-co <71203756+afreet-co@users.noreply.github.com>
2021-10-08 07:47:38 +0000

    Deploying to gh-pages from @ afreet-co/todo-app@f6ed1a01d4cfc8b4ce43b011be07cd8cc17dfa26 🚀

---
+++ _next/static/aem7sPfLAEqSJ6NqNDIq1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/3-714e51d5b671e2b8a3b9.js","static/chunks/pages/index-f465d67139c675744d55.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
